dac650c9b967b5b00c5e801e99ffe37db3ab09ad
Kohei Morita <mrtc0@ssrf.in>
2022-06-03 16:54:30 +0900

    fix: Coverd for git diff out of range
    Signed-off-by: Kohei Morita <mrtc0@ssrf.in>

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,6 +33,6 @@ if [ $namespaces ]; then
 fi
 
 trivy $TRIVY_ARGS . \
-  | jq '.runs[].results[] | "\(.level[0:1]):\(.locations[].physicalLocation.artifactLocation.uri):\(.locations[].physicalLocation.region.endLine) \(.message.text)"' \
+  | jq '.runs[].results[] | "\(.level[0:1]):\(.locations[].physicalLocation.artifactLocation.uri):\(.locations[].physicalLocation.region.startLine):\(.locations[].physicalLocation.region.endLine) \(.message.text)"' \
   | sed "s/\\\\n/<br>/g" \
-  | reviewdog -efm="\"%t:%f:%l %m\"" --diff="git diff ${GITHUB_REF}" -reporter=github-pr-review
+  | reviewdog -efm="\"%t:%f:%l:%e %m\"" --diff="git diff -U1000 ${GITHUB_REF}" -reporter=github-pr-review
