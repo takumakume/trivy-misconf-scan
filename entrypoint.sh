1c5233ec99bae2686e695527a2444f5f1efe31a8
5st7 <not404friends@gmail.com>
2022-06-06 12:35:16 +0900

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,6 +33,6 @@ if [ $namespaces ]; then
 fi
 
 trivy $TRIVY_ARGS . \
-  | jq '.runs[].results[]| if .locations[].physicalLocation.region.endLine then . else .locations[].physicalLocation.region.endLine=.locations[].physicalLocation.region.startLine end | "\(.level[0:1]):\(.locations[].physicalLocation.artifactLocation.uri):\(.locations[].physicalLocation.region.startLine):.\(.locations[].physicalLocation.region.endLine) \(.message.text)"' \
+  | jq '.runs[].results[]| if .locations[].physicalLocation.region.endLine then . else .locations[].physicalLocation.region.endLine=.locations[].physicalLocation.region.startLine end | "\(.level[0:1]):\(.locations[].physicalLocation.artifactLocation.uri):\(.locations[].physicalLocation.region.startLine):\(.locations[].physicalLocation.region.endLine) \(.message.text)"' \
   | sed "s/\\\\n/<br>/g" \
   | reviewdog -efm="\"%t:%f:%l:%e %m\"" --diff="git diff -U1000 ${GITHUB_REF}" -reporter=github-pr-review
